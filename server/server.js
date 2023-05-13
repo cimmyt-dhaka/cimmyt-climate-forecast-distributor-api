b3d797e2bda7dbcaf84ac48c9888ff330f7e6fc1
Mutasim Billah <mutasim.wre@gmail.com>
2023-05-13 11:49:07 +0600

    adjustments for cyclic

--- server/server.js
+++ server/server.js
@@ -9,25 +9,20 @@ const
   mungbean = require('./routes/api/mungbean.js'),
   faw = require('./routes/api/faw.js'),
 
-  app = express(),
-
-  appInit = async () => {
-    try {
-      await connect(mongoURI);
-      console.log("MongoDB connected");
-
-      app.listen(
-        port,
-        () => { console.log(`Server started on port ${ port }`); }
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  app = express();
 
 // app.use(cors());
 
 app.use('/api/mungbean', mungbean);
 app.use('/api/faw-forecasts', faw);
 
-appInit();
+connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(port, () => {
+      console.log(`Server started on port ${ port }`);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
