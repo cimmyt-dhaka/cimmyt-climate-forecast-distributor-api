6c4a95d0b9c0a55fa1ecfe579aa77a36c377529d
Mutasim Billah <mutasim.wre@gmail.com>
2025-04-01 14:53:37 +0600

    cors policy adjustment for observable's access

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const
-  // { join } = require('path'),
   express = require('express'),
+  cors = require('cors'),
   { connect } = require('mongoose'),
-  // cors = require('cors'),
   port = process.env.PORT || 5000,
 
   { mongoURI } = require('./config/keys.js'),
@@ -11,7 +10,7 @@ const
 
   app = express();
 
-// app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], allowedHeaders: ['Content-Type'] }));
 
 app.use('/api/mungbean', mungbean);
 app.use('/api/faw-forecasts', faw);
